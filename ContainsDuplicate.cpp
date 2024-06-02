18ed1d474c921ff38c6410e387c73a2f65613f04
Gaurav Shrivastava <gg283421@gmail.com>
2024-06-02 01:09:54 -0500

    final commit

--- ContainsDuplicate.cpp
+++ ContainsDuplicate.cpp
@@ -1,9 +1,32 @@
 #include<iostream>
+#include<set>
+#include<vector>
 
 using namespace std;
 
+class Solution{
+    public:
+    bool hasDuplicate(vector<int>& nums)
+    {
+        set<int> nums2(nums.begin(), nums.end());
+        if(nums2.size()==nums.size())
+        return false;
+        else 
+        return true;
+    }
+};
+
 int main()
 {
-
+    Solution solution;
+    vector<int> nums;
+    int n;
+    for(int i=0; i<4; ++i)
+    {
+        cin>>n;
+        nums.push_back(n);
+    }
+    cout<<solution.hasDuplicate(nums);
+   
     return 0;
 }
